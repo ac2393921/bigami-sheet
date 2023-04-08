fddd767709bddb738149ac1d546e52ca19c51fd4
ac2393921 <ac2393921@gmail.com>
2023-04-09 01:55:24 +0900

    update: サイドバーをコンポーネントに切り出し

---
+++ src/components/Sidebar/sidebar.tsx
@@ -0,0 +1,25 @@
+import { AiFillAppstore } from "react-icons/ai";
+import { FaUserNinja } from "react-icons/fa";
+
+export default function Sidebar() {
+  return (
+    <aside id="default-sidebar" className="fixed left-0 z-60 w-40 h-screen" aria-label="Sidebar">
+        <div className="h-full py-8 overflow-y-auto bg-stone-900 dark:bg-gray-800">
+            <ul className="space-y-2 font-medium">
+              <li>
+                  <a href="#" className="flex items-center p-2 text-gray-300 rounded-lg dark:text-white hover:bg-stone-700 dark:hover:bg-gray-700">
+                    <AiFillAppstore />
+                    <span className="ml-3">シート一覧</span>
+                  </a>
+              </li>
+              <li>
+                  <a href="#" className="flex items-center p-2 text-gray-300 rounded-lg dark:text-white hover:bg-stone-700 dark:hover:bg-gray-700">
+                    <FaUserNinja />
+                    <span className="ml-3">マイページ</span>
+                  </a>
+              </li>
+            </ul>
+        </div>
+      </aside>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import Header from "@/components/Header/header";
+import Sidebar from "@/components/Sidebar/sidebar";
 import Image from 'next/image';
-import { AiFillAppstore, AiFillCaretDown, AiFillDelete, AiFillPlusCircle, AiOutlineEdit, AiOutlineEye } from "react-icons/ai";
-import { FaUserNinja } from "react-icons/fa";
-import Header from "../components/Header/header";
+import { AiFillCaretDown, AiFillDelete, AiFillPlusCircle, AiOutlineEdit, AiOutlineEye } from "react-icons/ai";
 
 const characters = [
   { id: 1, name: 'shinobi1', image: '/images/比良坂機関.gif', school: '比良坂機関', class: '中忍', user: 'user1', created_time: '2023.4.8' },
@@ -16,30 +16,9 @@ const characters = [
 export default function Home() {
   return (
     <div>
-      {/* Header */}
       <Header />
-      {/* Header */}
 
-      {/* Sidebar */}
-      <aside id="default-sidebar" className="fixed left-0 z-60 w-40 h-screen" aria-label="Sidebar">
-        <div className="h-full py-8 overflow-y-auto bg-stone-900 dark:bg-gray-800">
-            <ul className="space-y-2 font-medium">
-              <li>
-                  <a href="#" className="flex items-center p-2 text-gray-300 rounded-lg dark:text-white hover:bg-stone-700 dark:hover:bg-gray-700">
-                    <AiFillAppstore />
-                    <span className="ml-3">シート一覧</span>
-                  </a>
-              </li>
-              <li>
-                  <a href="#" className="flex items-center p-2 text-gray-300 rounded-lg dark:text-white hover:bg-stone-700 dark:hover:bg-gray-700">
-                    <FaUserNinja />
-                    <span className="ml-3">マイページ</span>
-                  </a>
-              </li>
-            </ul>
-        </div>
-      </aside>
-      {/* Sidebar */}
+      <Sidebar />
 
       {/* Main */}
       <div className="bg-[url('/images/bg.jpg')] h-screen p-4 sm:ml-32">
